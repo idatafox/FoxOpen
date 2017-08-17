9658424a3b2ee3ea66285c84f9647155ea1b9594
idatafox <wishlong@yahoo.com>
2017-08-17 22:54:35 +0800

    testA

--- app/src/main/java/com/idatafox/foxopen/NextActivity.java
+++ app/src/main/java/com/idatafox/foxopen/NextActivity.java
@@ -3,7 +3,10 @@ package com.idatafox.foxopen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -17,5 +20,26 @@ public class NextActivity extends AppCompatActivity {
     public void openEditTextDemoWin(View v){
         Intent intentObj=new Intent(this,EditTextDemoA.class);
         startActivity(intentObj);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.abc,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_read:
+                Toast.makeText(getApplicationContext(),"read...",Toast.LENGTH_SHORT).show();
+                 return true;
+            case R.id.action_del:
+                Toast.makeText(getApplicationContext(),"delete...",Toast.LENGTH_SHORT).show();
+                 return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/idatafox/foxopen/TopNewsReporter.java
+++ app/src/main/java/com/idatafox/foxopen/TopNewsReporter.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -209,4 +211,39 @@ public class TopNewsReporter extends AppCompatActivity implements OnItemClickLis
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.abc,menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        return true;
+    }
+
 }
+
+
+/*
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<menu xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    tools:context="com.idatafox.foxopen.TopNewsReporter"
+    >
+
+    <item
+        android:id="@+id/action_search"
+        android:icon="@mipmap/ic_launcher"
+        android:title="search"
+        app:showAsAction="always" />
+
+</menu>
+
+
+ */
